d7a3ec969ced203489162f96d7da1e4ac478f90a
Mattias <mattisamaasik@gmail.com>
2022-09-27 12:13:55 +0300

    s

--- index.php
+++ index.php
@@ -2,14 +2,33 @@
 
 require_once('connection.php');
 
+
 echo '<ul>';
 
-$stmt = $pdo->query('SELECT * FROM books');
-while ($row = $stmt->fetch())
 
+?>
 
-{
-    echo '<li><a href="book.php?id=' . $row['id'] . '">' . $row['title'] . '</li>' ;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="indexstyle.css">
+    <title>Document</title>
+</head>
+<body>
 
-echo '</ul>';
+    <?php
+
+        $stmt = $pdo->query('SELECT * FROM books');
+        while ($row = $stmt->fetch())
+        { ?>
+            <li><a href="book.php?id=<?php echo $row['id']; ?>"><h1><?php echo $row['title']; ?></h1></li>
+        <?php
+        }
+
+    ?>
+
+</body>
+</html>
